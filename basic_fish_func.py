2c00b689c9aec9cc2f2340fc971a7c9e2b5b3290
pook1r <35053258+pook1r@users.noreply.github.com>
2018-01-15 17:30:33 -0300

    Add files via upload
    Added in variables for the different fish, but it seems to reset each time I run the main function.
    
    Perhaps I need to use CSV to store the different fish values?

--- basic_fish_func.py
+++ basic_fish_func.py
@@ -1,13 +1,18 @@
 import random
 import numpy as np
 
+global mackerel
+global cockle
+global s_karp
 
-a = ""
-encounter = ""
 
 
 def fish_func_def (fishing, a):  #Defines the fishing function
-    fishing = input("""
+    mackerel = 0
+    cockle = 0
+    s_karp = 0
+
+    fishing = raw_input("""
 Did you catch a fish?
 
 1. Yes
@@ -16,26 +21,32 @@ Did you catch a fish?
 
 Enter the corresponder number: """)
 
-    if fishing == 1:
-        a = random.randrange(1,10)
+    if fishing == "1":
+        a = random.randint(1,10)
         print a
-        if a in range(1,3):
-            print"You caught a mackerel. Can be caught at lvl 10 in Runescape."
-        if a in range(3,7):
+        if a in range(1,4):
+            mackerel +=1
+            print"You caught a mackerel. Can be caught at lvl 16 in Runescape."
+            print"You have ",mackerel," mackerel."
+        elif a in range(4,7):
             print"You caught a cockle. The cockle is a native to Goolwa, where you went as a child."
-        if a in range(7,9):
+            cockle +=1
+            print"You have ",cockle," cockle."            
+        elif a in range(7,10):
             print"You found nothing unfortunately :("
-        if a == 10:
+        elif a == 10:
+            s_karp +=1
             print"You caught a shiny Magikarp :D You won the lottery!"
+            print"You have",s_karp,"shiny Magikarp."    
         else:
             fish_func_def ("","")
-    elif fishing ==2:
+    elif fishing =="2":
         print "Party pooper. Perhaps try a different option?"
         fish_func_def ("","")
-    elif fishing ==3:
+    elif fishing =="3":
         print "You pressed 3"
 
     else:
-        fishing = input("That is not a valid selection. Please try again: ")
+        fishing = raw_input("That is not a valid selection. Please try again: ")
 
 fish_func_def("","") #call the fish_func_def function that we defined earlier

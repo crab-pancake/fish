19eddc65c79aa73c98e5f69c62b597d1d5b8cc5e
bulletbrainsurgery <30866670+bulletbrainsurgery@users.noreply.github.com>
2018-02-14 21:51:25 +1100

    Add files via upload

--- fish2.py
+++ fish2.py
@@ -17,18 +17,14 @@ def acct_ask(): #This is complete
 def new_acct():
   uname = input("Select new username:\n>> ")
   try:
-    playerfile = open(uname+'.csv', 'r')
-    print ('Account already exists.')
-    playerfile.close()
-    acct_ask()
-  except IOError:
-    print ('Creating account with username %s...' % (uname))
-    with open(uname+'.csv', 'w') as playerfile:#+'_g_info.csv'
-      writer = csv.writer(playerfile, dialect = 'excel')
-      writer.writerow(['Create Time', time.time()])
-      writer.writerow(['Last Login', 0]) #changed lastlogin to set to 0 when 
+    with open(uname+'.csv', 'x', newline = '') as playerfile:#+'_g_info.csv'
+      print ('Creating account with username %s...' % (uname))
+      fieldnames = ['Create Time', 'Last Login', 'Password']
+      writer = csv.writer(playerfile, dialect='excel')
+      writer.writerow(['Create Time', str(time.time())])
+      writer.writerow(['Last Login',0]) #changed lastlogin to set to 0 when acct is created
       while True:
-        pw = rinput("Enter a password longer than 3 characters, or type 'back' to cancel.\n>>")
+        pw = input("Enter a password longer than 3 characters, or type 'back' to cancel.\n>>")
         if pw.lower() == 'back':
           acct_ask()
           break
@@ -37,16 +33,19 @@ def new_acct():
         else:
           pwconfirm = input("Confirm password.\n>>")
           if pw == pwconfirm:
-            writer.writerow(['Password', pw])
+            writer.writerow(['Password',pw])
             print ('Password confirmed, please log in to your account.')
             playerfile.close() #closing first will write the stuff to the file before log_in is called, avoids indexerror
             log_in()
             break
           else:
             print ('Sorry, the passwords didn\'t match.')
+  except FileExistsError:
+    print ('Account already exists.')
+    acct_ask()
 
 def log_in():
-  global uname #what does this do?
+  #global uname #what does this do?
   uname = input("Enter your account name to login.\n>> ")
   print ('Logging in to account %s...' % (uname))
   try:
@@ -55,8 +54,8 @@ def log_in():
     with open(uname+'.csv', 'r') as playerfile:
       reader = csv.reader(playerfile)
       stats = dict(reader)
-      password=stats["Password"]#[row for i, row in enumerate(reader) if i == 2][0][1]
-      print (password)
+      password=stats['Password']
+      print ('Password:',password)
       pword = input("Please type your password.\n>> ")
       if password == pword:
         print ('Successfully logged in to account %s.' % (uname))

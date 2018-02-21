c224509842acb9df697ad76a82f16af90b778d80
bulletbrainsurgery <30866670+bulletbrainsurgery@users.noreply.github.com>
2018-02-21 20:15:25 +1100

    added strip() to uname inputs

--- login.py
+++ login.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def acct_ask(): #This is complete
   while True:
-    exist = input("Do you already have an account? (Y/N)\n>>" ).lower()
+    exist = input("Do you already have an account? (Y/N)\n>>" ).strip().lower()
     if exist=='y' or exist=='ye'or exist=='yes' or exist=='1':
       log_in()
       break
@@ -16,12 +16,12 @@ def acct_ask(): #This is complete
       print ('Invalid response, please try again.\n')
 
 def new_acct():
-  uname = input("Select new username:\n>> ")
+  uname = input("Select new username:\n>> ").strip()
   try:
     with open(uname+'_i.csv', 'x', newline = '') as playerfile:
       print ('Creating account with username %s...' % (uname))
       writer = csv.writer(playerfile, dialect='excel')
-      writer.writerow(['Create Time', str(time.time())])
+      writer.writerow(['Create Time', time.time()])
       writer.writerow(['Last Login',0]) #changed lastlogin to set to 0 when acct is created
       while True:
         pw = input("Enter a password longer than 3 characters, or type 'back' to cancel.\n>>")
@@ -33,29 +33,26 @@ def new_acct():
         else:
           pwconfirm = input("Confirm password.\n>>")
           if pw == pwconfirm:
-            writer.writerow(['Password',pw])
+            writer.writerow(['Password',pw]) #change this to write hash of password: import hashlib, find out how this works
             print ('Password confirmed, please log in to your account.')
             playerfile.close() #closing first will write the stuff to the file before log_in is called, avoids indexerror
             log_in()
             break
           else:
-            print ('Sorry, the passwords didn\'t match.')
+            print ("Sorry, the passwords didn't match.")
   except FileExistsError:
-    print ('Account already exists.')
+    print ('Account with the username %s already exists.' % (uname))
     acct_ask()
 
 def log_in():
-  uname = input("Enter your account name to login.\n>> ")
+  uname = input("Enter your account name to login.\n>> ").strip()
   print ('Logging in to account %s...' % (uname))
   acc_path = Path("./%s_i.csv" % (uname))  #put this into lower line?
   if acc_path.is_file():
-    with open(uname+'_i.csv', 'r') as playerfile:
-      reader = csv.reader(playerfile)
-      stats = dict(reader)
-      password=stats['Password']
-      print ('Password:',password)
-      pword = input("Please type your password.\n>> ")
-      if password == pword:
+    with open(uname+'_i.csv', 'r') as file:
+      stats = dict(csv.reader(file))
+      pw = input("Please type your password.\n>> ")
+      if pw == stats['Password']:
         print ('Successfully logged in to account %s.' % (uname))
         exec(open('./gamecode.py').read())
       else:

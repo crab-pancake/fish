78df0bc0797a85519ae8f84f1f8936a82bbb87b9
bulletbrainsurgery <30866670+bulletbrainsurgery@users.noreply.github.com>
2018-02-22 11:49:00 +1100

    Updated files to match current allitems format

--- gamecode.py
+++ gamecode.py
@@ -24,63 +24,62 @@ class User_s(object):
             for key, value in stats.items():
                 writer.writerow([key, value])
 
-
 class Item(object):
     """items"""
-    def __init__(self, code, name, description, exp, min_level):
+    def __init__(self, code, item_name, description, exp, min_level):
         self.code = code
-        self.name = name
+        self.item_name = item_name
         self.description = description
         self.exp = exp
         self.min_level = min_level
         self.type = 'other'
     def __str__(self):
-        return "Item with code %s, name %s" % (self.code, self.name)
+        return "Item with code %s, name %s" % (self.code, self.item_name)
     def __repr__(self):
-        return "Item(self, %r, %r, %r, %r, %r)" % (self.code, self.name, self.description, self.exp, self.min_level)
+        return "Item(self, %r, %r, %r, %r, %r)" % (self.code, self.item_name, self.description, self.exp, self.min_level)
 
 class Fish(Item):
-    def __init__(self, code, name, description, exp, min_level):
-        super().__init__(code, name, description, exp, min_level)
+    def __init__(self, code, item_name, description, exp, min_level):
+        super().__init__(code, item_name, description, exp, min_level)
         self.type = 'fish'
         self.sell_price = 5
     def __str__(self):
-        return "Fish item with code %s, name %s" % (self.code, self.name)
+        return "Fish item with code %s, name %s" % (self.code, self.item_name)
     def __repr__(self):
-        return "Fish(self, %r, %r, %r, %r, %r)" % (self.code, self.name, self.description, self.exp, self.min_level)
+        return "Fish(self, %r, %r, %r, %r, %r)" % (self.code, self.item_name, self.description, self.exp, self.min_level)
 
 class Bait(Item):
-    def __init__(self, code, name, description, exp, min_level):
-        super().__init__(code, name, description, exp, min_level)
+    def __init__(self, code, item_name, description, exp, min_level):
+        super().__init__(code, item_name, description, exp, min_level)
         self.type = 'bait'
     def __str__(self):
-        return "Bait item with code %s, name %s" % (self.code, self.name)
+        return "Bait item with code %s, name %s" % (self.code, self.item_name)
     def __repr__(self):
-        return "Bait(self, %r, %r, %r, %r, %r)" % (self.code, self.name, self.description, self.exp, self.min_level)
+        return "Bait(self, %r, %r, %r, %r, %r)" % (self.code, self.item_name, self.description, self.exp, self.min_level)
 
 class Material(Item):
-    def __init__(self, code, name, description, exp, min_level):
-        super().__init__(code, name, description, exp, min_level)
+    def __init__(self, code, item_name, description, exp, min_level):
+        super().__init__(code, item_name, description, exp, min_level)
         self.type = 'material'
     def __str__(self):
-        return "Material item with code %s, name %s" % (self.code, self.name)
+        return "Material item with code %s, name %s" % (self.code, self.item_name)
     def __repr__(self):
-        return "Material(self, %r, %r, %r, %r, %r)" % (self.code, self.name, self.description, self.exp, self.min_level)
+        return "Material(self, %r, %r, %r, %r, %r)" % (self.code, self.item_name, self.description, self.exp, self.min_level)
 
 global ListOfItems
 ListOfItems = {}
 print('listofitems has been made')
-with open('test.csv', 'r') as readfile:
+with open('allitems.csv', 'r') as readfile:
     reader = csv.DictReader(readfile)
     for row in reader:
-        # if row['i_type'] == 'fish':
-        #     ThisItem = Fish(row['code'], row['name'], row['description'], row['exp'], row['min_level'])
-        #     ListOfItems[ThisItem.code] = ThisItem
-        # elif row['i_type'] == 'bait':
-        #     ThisItem = Bait(row['code'], row['name'], row['description'], row['exp'], row['min_level'])
-        #     ListOfItems[ThisItem.code] = ThisItem
-        # else:
-        ThisItem = Item(row['code'], row['name'], row['description'], row['exp'], row['min_level'])
+        if row['i_type'] == 'fish':
+            ThisItem = Fish(row['code'], row['item_name'], row['description'], row['exp'], row['min_level'])
+            ListOfItems[ThisItem.code] = ThisItem
+        elif row['i_type'] == 'bait':
+            ThisItem = Bait(row['code'], row['item_name'], row['description'], row['exp'], row['min_level'])
+            ListOfItems[ThisItem.code] = ThisItem
+        else:
+            ThisItem = Item(row['code'], row['item_name'], row['description'], row['exp'], row['min_level'])
         ListOfItems[ThisItem.code] = ThisItem
 
 class User_g(object):
@@ -107,21 +106,21 @@ class User_g(object):
             while True:
                 fish = input("Would you like to fish? Press Y for yes, N for no.\n>> ").strip().lower()
                 if fish == "y":
-                    if self.inv['000'] >0:
-                        self.inv['000'] -= 1
+                    if self.inv['00000'] >0:
+                        self.inv['00000'] -= 1
                         rng = random.randint(1,100)
                         catch = False
                         for key in self.table:
                             if rng >= int(self.table[key]):
                                 self.inv[key] =  int(self.inv[key]) +1
-                                print("You caught a [",self.allitems[key].name,'] .')
-                                print("You have",self.inv[key],self.allitems[key].name+'.')
+                                print("You caught a [",self.allitems[key].item_name,'] .')
+                                print("You have",self.inv[key],self.allitems[key].item_name+'.')
                                 self.suc_fish(key)
                                 catch = True
                                 break
                         if catch == False:
                             print("You didn't catch anything.")
-                            self.suc_fish('000')
+                            self.suc_fish('00000')
                     else:
                         print ("You have no units of fishing juice left. Try waiting at least another hour.")
                 elif fish == "n":
@@ -129,7 +128,7 @@ class User_g(object):
                 else:
                     print ("Invalid response, try again.\n")
     def suc_fish(self, fish):
-        print ("You have",self.inv['000'],"fishing juice remaining.")
+        print ("You have",self.inv['00000'],"fishing juice remaining.")
         player_s.exp = int(player_s.exp)
         player_s.exp += int(self.allitems[fish].exp)
         self.save()
@@ -139,9 +138,10 @@ class User_g(object):
     def help_display(self):
         print("\nHELP\n"
             "\n----------------------------\n"
-            "\nHistory:\nJerry and Dayu thought of this game as they were walking with Evan and Mummy, along Lake Nordonskjold, W-Trek, Patagonia, Chile in late December 2017. The inspiration came from many hours of idle chat, but at least it encouraged them to do somethiing productive!\n"
+            "\nHistory:\nJerry and Dayu thought of this game as they were walking with Evan and Mummy, along Lake Nordenskjoeld, W-Trek, Patagonia, Chile in late December 2017. The inspiration came from many hours of idle chat, but at least it encouraged them to do somethiing productive!\n"
             "\nAim:\nThis is a time based game, where you as the player character gather 'fishing juice' to catch fish, upgrade your setup and further your fishing capabilities.\n"
-            "\nInitial setup:\nYou'll begin with 10 fishing juice and no fish. Under the 'Menu' option, choose 'Fish!' to use up your fishing juice and catch fish. Each time you'll have a go at fishing and deplete your fishing juice by one. You'll gather more fishing juice by logging off (1 per hour is the base rate) and relogging on.\n"
+            "\nInitial setup:\nYou'll begin with 10 fishing juice and no fish. Under the 'Menu' option, choose 'Fish!' to use up your fishing juice and catch fish.\n"
+            "Each time you'll have a go at fishing and deplete your fishing juice by one. You'll gather more fishing juice by logging off (1 per hour is the base rate) and relogging on.\n"
             "\nBuying and selling:\n Enter the corresponding number 'Visit shop' in order to buy and sell your fish to gain gold. Use your gold to upgrade your fishing set up. For example, the cheapest upgrade will is the 'Reinforced net', which will increase your fishing juice gathering rate by 10%.\n"
             "\nFinal words:\n Good luck! We'll be slowly adding in extra features, but be patient as we are new :3")
     def display_menu(self):
@@ -173,7 +173,7 @@ class User_g(object):
     def inv_display(self): 
         print("YOUR INVENTORY:\n-------------------------")
         for key in self.inv:
-            print('%s: %s' % (self.allitems[key].name, self.inv[key]))
+            print('%s: %s' % (self.allitems[key].item_name, self.inv[key]))
 
 global player_s
 player_s = User_s(playerfile["Create Time"], playerfile["Last Login"], playerfile["Password"], uname, playerfile["exp"])
@@ -183,7 +183,7 @@ if player_s.p_time_raw == '0': #If this is the first access of the game, then pt
     player_s.update_time()
     player_s.save()
     player_g = User_g(uname)
-    player_g.inv['000'] = 10  #when a new account is created, 10 fishing juice is given
+    player_g.inv['00000'] = 10  #when a new account is created, 10 fishing juice is given
     print ("Welcome to the game! This is your first login. \n"
            "Account creation time: %s"%(player_s.f_time))
     player_g.display_menu()
@@ -195,7 +195,7 @@ elif player_s.delta > 0:
     player_s.update_time()
     player_s.save()
     player_g.load()
-    player_g.inv['000']=int(player_g.inv['000'])+int(player_s.hsll)
+    player_g.inv['00000']=int(player_g.inv['00000'])+int(player_s.hsll)
     print ("___________________\n"
            "Welcome back to the game!\n"
            "Account creation time: %s\n"

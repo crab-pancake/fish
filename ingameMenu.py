1432c06cbc2cb7feaa944aba8cbc9815e8869182
pook1r <35053258+pook1r@users.noreply.github.com>
2018-03-06 23:13:46 +1100

    Add files via upload

--- ingameMenu.py
+++ ingameMenu.py
@@ -2,56 +2,46 @@ import universals as univ
 import json
 import time
 
-def menu(account):
-    menu_options = ["Display Inventory","Display Position","Other Info","Save","Exit"]
+def menu(player):
+    menu_options = {1:("Display Inventory",disp_inventory), 2:("Display Position",disp_position), 3:("Other Info",other_info), 4:("Save",save), 5:("Return to game",return_to_game), 0:("Exit Game",exit)}
     while True:
         print("\n~~~MENU~~~\n")
-        for i,value in enumerate(menu_options,1):
-        	print(str(i)+". "+value)
-        menu_input = int(input("\nEnter the corresponding number: >>"))
-        if menu_options[menu_input-1] == "Display Inventory":
-            disp_inventory()
-        if menu_options[menu_input-1] == "Display Position":
-            disp_position()
-        if menu_options[menu_input-1] == "Other Info":
-            other_info()
-        if menu_options[menu_input-1] == "Save":
-            player.save()
-        if menu_options[menu_input-1] == "Exit":
-            break
+        for key in menu_options:
+        	print(str(key)+". "+menu_options[key][0])
+        print("\nEnter the corresponding number:\n")
+        choice = univ.IntChoice(len(menu_options),[],[0])
+        var = menu_options[choice][1](player)
+        if var:
+            return var
 
-def disp_inventory():
+def disp_inventory(player):
     print("\n~~~YOUR INVENTORY~~~\n")
     for key in player.inventory:
         print(univ.ListOfItems[key].description +": " + str(player.inventory[key]))
 
-def disp_position():
+def disp_position(player):
     print("\n~~~YOUR POSITION~~~\n"
         "Position:" + player.position)
 
-def other_info():
+def other_info(player):
     print("\n~~~OTHER INFO~~~\n"
         "\nCreate Time: %s"
         "\nLast Login Time: %s" 
         "\nExperience: %s" % (time.strftime('%d-%m-%Y %H:%M:%S',time.localtime(player.createtime)), time.strftime('%d-%m-%Y %H:%M:%S',time.localtime(player.lastlogin)), player.exp['fishing']))
 
-def save():
-    print("\nSaving...\n")
-    stats = {
-    "username": player.username,
-    "password": player.password,
-    "createtime": player.createtime,
-    "lastlogin": player.lastlogin,
-    "exp": player.inventory,
-    "inventory": player.inventory,
-    "position": player.position
-    }
-    with open('./PlayerAccts/'+player.username+'_p.json', 'w') as file:
-        json.dump(stats, file)    
-    print("Save complete.")
+def save(player):
+    print('saved')
+    player.save()
 
-def exit():
+def exit(player):
     print("\nExiting program...")
+    import gamemap.quit
+    return (player, gamemap.quit)
+
+def return_to_game(player):
+    print("\nReturning to game...")
+    import gamemap.displayPlaces
+    return (player, gamemap.displayPlaces)
 
 if __name__ == "__main__":
     reader=''

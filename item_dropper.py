0cab528e4b2e7b8644f08e724fc8040a86f05160
pook1r <35053258+pook1r@users.noreply.github.com>
2018-03-08 22:14:44 +1100

    updated dropper. Has hapalnoacha egg

--- item_dropper.py
+++ item_dropper.py
@@ -1,16 +1,36 @@
 import random as rand
 
-def drop_item():
-    rare_bound = 0.1
-    var = rand.random()
-    if var <= 0.1 *rare_bound:
-	    print("Rare item!")
-    elif var <= rare_bound:
-        print("Uncommon item.")
-    else:
-        print(var)
-        print("Common item.")
+fishing_drop ={
+1:["Treasure Chest", "Native pearl", "Tetrodotoxin cubes", "Hapalochleana egg"],
+2:["Gummy sharkling", "Electric eel", "Flounder", "Magikarp", "Feebas"],
+3:["Pufferfish", "Mackerel", "Shrimp", "Snapper", "Bass"]
+}
 
+rarity = [
+"Common",
+"Uncommon",
+"Rare",
+"Ultra rare",
+"Unbelievably rare"
+]
+
+def drop_item(skill, *bound):
+    while True:
+        fish = input("Would you like to fish?")
+        if fish == 'y':    
+            var = rand.random()
+            for a in range (1,len(bound)+1):
+                if var <= sum(bound[:a]):
+                    print("You got a %s. This is a %s item."  % (rand.choice(skill[a]), rarity[len(bound)-a].lower()))
+                    break
+
+    # if var <= bound[0]:
+    #     print("Rare item!")
+    # elif var <= rare_bound:
+    #     print("Uncommon item.")
+    # else:
+    #     print(var)
+    #     print("Common item.")
 
 if __name__ == '__main__':
-    drop_item()
+    drop_item(fishing_drop, 0.08, 0.09, 0.9)

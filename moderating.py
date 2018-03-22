5ec6a94724844592e4426d7a0b61fe5c52d3c2e1
bulletbrainsurgery <30866670+bulletbrainsurgery@users.noreply.github.com>
2018-03-22 22:11:14 +1100

    Add files via upload

--- moderating.py
+++ moderating.py
@@ -14,37 +14,40 @@ def prettysave(filename):
     with open(filename+'.json', 'w') as file:
         json.dump(parsed, file, indent=2, separators=(',', ': '))
     print('Saved prettily')
-
+    
 def update(username):
-    with open('./PlayerAccts/%s_p.json'%(username), 'r') as file:
-        reader = json.load(file)
-        try:
-            reader['createtime']=int(reader['create time'])
-            reader['lastlogin']=int(reader['last login'])
-        except KeyError:
-            reader['createtime']=int(reader['createtime'])
-            reader['lastlogin']=int(reader['lastlogin'])
-        try:
-            if not reader['equipment']:
+    try:
+        with open('./PlayerAccts/%s_p.json'%(username), 'r') as file:
+            reader = json.load(file)
+            try:
+                reader['createtime']=int(reader['create time'])
+                reader['lastlogin']=int(reader['last login'])
+            except KeyError:
+                reader['createtime']=int(reader['createtime'])
+                reader['lastlogin']=int(reader['lastlogin'])
+            try:
+                if not reader['equipment']:
+                    reader['equipment']=dict.fromkeys(range(1,10),None)
+            except KeyError:
                 reader['equipment']=dict.fromkeys(range(1,10),None)
-        except KeyError:
-            reader['equipment']=dict.fromkeys(range(1,10),None)
-        player = univ.Player(**reader)
-        newdict = {}
-        for key in player.inventory:
-            if key[0] == 'i':
-                newdict[key]=player.inventory[key]
-            else:
-                newdict['i'+key]=player.inventory[key]
-        for item in univ.ListOfItems.keys():
-            newdict[item]=player.inventory.get(item,0)
-        player.inventory=newdict
-        for skill in univ.skills:
-            player.exp[skill] = player.exp.get(skill, 0)
-        if len(player.password)!=64:
-            player.password=hashlib.sha256(player.password.encode('utf-8')).hexdigest()
-        if len(player.position)!=3:
-            player.position='000'
-        player.save()
-    print("Account updated.")
+            player = univ.Player(**reader)
+            newdict = {}
+            for key in player.inventory:
+                if key[0] == 'i':
+                    newdict[key]=player.inventory[key]
+                else:
+                    newdict['i'+key]=player.inventory[key]
+            for item in univ.ListOfItems.keys():
+                newdict[item]=player.inventory.get(item,0)
+            player.inventory=newdict
+            for skill in univ.skills:
+                player.exp[skill] = player.exp.get(skill, 0)
+            if len(player.password)!=64:
+                player.password=hashlib.sha256(player.password.encode('utf-8')).hexdigest()
+            if len(player.position)!=3:
+                player.position='000'
+            player.save()
+        print("Account updated.")
+    except FileNotFoundError as e:
+        raise e
 t='test_acct'
